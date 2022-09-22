4f457eea8cdc7225220131bb5b5c2291b8b1b560
makelloss <artemvakhno@gmail.com>
2022-09-22 22:30:07 +0300

    add

--- index.php
+++ index.php
@@ -1,9 +1,12 @@
 <?php
 $t = rand(-20,20);
-$i = 0;
 print "<table style=\"border:solid;\">";
-$style = $t > $i ? 'width:30px;background:red' : 'width:30px;background:yellow';
+
+for ($i = 20; $i > -1; $i--) {
+    $style = $t > $i ? 'width:30px;background:red' : 'width:30px;background:blue';
+}
 for ($t = -20; $t <= 20; $t++) {
-    print "<tr><td style=\"border:solid;\">$t</td><td style=\"border:solid;\" style=\"{$style}\"\></td></tr>";
+    $style = $t > $i ? 'width:30px;background:red' : 'width:30px;background:blue';
+    print "<tr><td style=\"border:solid;\">$t</td><td style=\"{$style}\"\></td></tr>";
 }
 print "</table>";
