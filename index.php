48e162995cde38c89b23ddba7fdd7bf1ccd6d44a
makelloss <artemvakhno@gmail.com>
2022-09-23 21:12:03 +0300

    second commit

--- index.php
+++ index.php
@@ -1,12 +1,7 @@
 <?php
 $t = rand(-20,20);
 print "<table style=\"border:solid;\">";
-
-for ($i = 20; $i > -1; $i--) {
-    $style = $t > $i ? 'width:30px;background:red' : 'width:30px;background:blue';
-}
-for ($t = rand(-20,20); $t <= 20; $t++) {
+for ($i = 1; $i > 0; $i--) {
     $style = $t > $i ? 'width:30px;background:red' : 'width:30px;background:blue';
     print "<tr><td style=\"border:solid;\">$t</td><td style=\"{$style}\"\></td></tr>";
 }
-print "</table>";
