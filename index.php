2db514b18b362716b4d8bd9fd13c4013097e730e
makelloss <artemvakhno@gmail.com>
2022-09-23 22:43:29 +0300

    done

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
-$t = rand(-20,20);
+$i = 0;
 print "<table style=\"border:solid;\">";
-for ($i = 20; $i > -20; $i--) {
+for ($t = 20; $t > -20; $t--) {
     $style = $t > $i ? 'width:30px;background:red' : 'width:30px;background:blue';
     print "<tr><td style=\"border:solid;\">$t</td><td style=\"{$style}\"\></td></tr>";
 }
